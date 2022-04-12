302122ab7b2533b6b3797239d258dc648e630adf
Berkay Tekin Oz <berkay.tekinoz@canonical.com>
2022-04-12 17:10:02 +0300

    Added database relation with mysql

--- src/charm.py
+++ src/charm.py
@@ -14,8 +14,9 @@ develop a new k8s charm using the Operator Framework:
 
 import logging
 
-from ops.charm import CharmBase
+from ops.charm import CharmBase, RelationChangedEvent, RelationBrokenEvent
 from ops.main import main
+from ops.framework import StoredState
 from ops.model import ActiveStatus, WaitingStatus
 
 logger = logging.getLogger(__name__)
@@ -24,11 +25,16 @@ logger = logging.getLogger(__name__)
 class SampleWorkloadCharm(CharmBase):
     """Charm the service."""
 
+    _stored = StoredState()
+
     def __init__(self, *args):
         super().__init__(*args)
         self.framework.observe(self.on.wordpress_pebble_ready, self._on_wordpress_pebble_ready)
         self.framework.observe(self.on.config_changed, self._on_config_changed)
         self.framework.observe(self.on.fortune_action, self._on_fortune_action)
+        self.framework.observe(self.on.mysql_relation_changed, self._on_mysql_relation_changed)
+        self.framework.observe(self.on.mysql_relation_broken, self._on_mysql_relation_broken)
+        self._stored.set_default(db_config={"name": "", "host": "", "password": "", "user": ""})
 
     def _on_wordpress_pebble_ready(self, event):
         """Define and start a workload using the Pebble API.
@@ -62,7 +68,13 @@ class SampleWorkloadCharm(CharmBase):
                     "summary": "wordpress",
                     "command": "docker-entrypoint.sh apache2-foreground",
                     "startup": "enabled",
-                    "environment": {"WP_DEBUG": self.model.config["wp-debug"]},
+                    "environment": {
+                        "WP_DEBUG": self.model.config["wp-debug"],
+                        "WP_DATABASE_HOST": self._stored.db_config["host"],
+                        "WP_DATABASE_USER": self._stored.db_config["user"],
+                        "WP_DATABASE_PASSWORD": self._stored.db_config["password"],
+                        "WP_DATABASE_NAME": self._stored.db_config["name"]
+                        },
                 }
             },
         }
@@ -89,6 +101,27 @@ class SampleWorkloadCharm(CharmBase):
             self.unit.status = ActiveStatus()
         else:
             self.unit.status = WaitingStatus("waiting for Pebble in workload container")
+
+    def _on_mysql_relation_changed(self, event: RelationChangedEvent) -> None:
+
+        if event.unit:
+            logger.info(event.unit)
+            logger.info(event.relation.data[event.unit])
+            database = event.relation.data[event.unit].get("database")
+            host = event.relation.data[event.unit].get("host")
+            password = event.relation.data[event.unit].get("password")
+            user = event.relation.data[event.unit].get("user")
+            self._stored.db_config.update({"name": database, "host": host, "password": password, "user": user})
+        
+            logger.info("New database configuration {}".format(self._stored.db_config))
+ 
+        self._on_config_changed(event)
+
+    def _on_mysql_relation_broken(self, event: RelationBrokenEvent) -> None:
+        # Remove the unit data from local state
+        self._stored.db_config.update({"name": "", "host": "", "password": "", "user": ""})
+        # Do something
+        self._on_config_changed(event)
 
     def _on_fortune_action(self, event):
         """Just an example to show how to receive actions.
